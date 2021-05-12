2071c58cd8fa3c8328f294209165722f5869ee90
Ruben Amorim <ruben.amorim@mindera.com>
2021-05-12 12:56:03 +0100

    update personal website configs

--- _data/sites/rubenamorim.js
+++ _data/sites/rubenamorim.js
@@ -1,6 +1,5 @@
 module.exports = {
 	name: "rubenamorim.dev", // optional, falls back to object key
-	description: "Ruben’s Personal web site",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
