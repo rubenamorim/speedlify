1c7a96aaa9d36df29599d2d6b10d6f58de005993
Ruben Amorim <ruben.amorim@mindera.com>
2021-05-12 12:51:04 +0100

    add first configs

---
+++ _data/sites/rubenamorim.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "rubenamorim.dev", // optional, falls back to object key
+	description: "Ruben’s Personal web site",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://rubenamorim.dev/"
+	]
+};
