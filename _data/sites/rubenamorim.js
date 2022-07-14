a89b7a7dd7540ddf0064b1a13a016307418c90e3
Ruben Amorim <me@rubenamorim.dev>
2022-07-14 16:16:31 +0100

    add bikeservice configs

--- _data/sites/rubenamorim.js
+++ _data/sites/rubenamorim.js
@@ -1,9 +1,7 @@
 module.exports = {
-	name: "rubenamorim.dev", // optional, falls back to object key
-	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
-	},
-	urls: [
-		"https://rubenamorim.dev/"
-	]
+  name: "rubenamorim.dev", // optional, falls back to object key
+  options: {
+    frequency: 60 * 23, // (in minutes), 23 hours
+  },
+  urls: ["https://rubenamorim.dev/"],
 };
