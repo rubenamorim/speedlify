4ab7ac2dfdacf3083b95d780e14d966b4d56fc79
Ruben Amorim <me@rubenamorim.dev>
2022-07-15 15:14:00 +0100

    add more bikeservice pages

--- _data/sites/bikeservice.js
+++ _data/sites/bikeservice.js
@@ -5,10 +5,16 @@ module.exports = {
   },
   urls: [
     "https://bikeservice.pt/",
-    "https://bikeservice.pt/page/sobre/",
+    "https://bikeservice.pt/event/braganca-granfondo/",
     "https://bikeservice.pt/event/corrida-de-sao-pedro/",
+    "https://bikeservice.pt/event/corrida-dos-reis/",
+    "https://bikeservice.pt/event/douro-granfondo/",
+    "https://bikeservice.pt/event/geres-granfondo/",
     "https://bikeservice.pt/event/maratona-btt-da-povoa/",
     "https://bikeservice.pt/event/moncao-e-melgaco-granfondo/",
+    "https://bikeservice.pt/event/montemuro-granfondo/",
+    "https://bikeservice.pt/event/raid-das-masseiras/",
     "https://bikeservice.pt/event/viana-granfondo/",
+    "https://bikeservice.pt/page/sobre/",
   ],
 };
